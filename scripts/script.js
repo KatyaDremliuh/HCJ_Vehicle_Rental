d77686597ac3d8844b93e5c2f4b0317b033b70c0
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-08-06 12:48:44 +0300

    Add script for input and background

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,15 @@ document.getElementById("price-action").onclick = function () {
         alert(successfully);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-image');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.7) / 9) + 'px,' + ((event.clientY * 0.7) / 9) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.7 * window.scrollY) + 'px';
+    })
+});
